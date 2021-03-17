536aafbeadaf2d82d1ff2460e8265b5c893d14b9
Azur Ramic <azurramic@gmail.com>
2021-03-17 10:20:39 +0100

    Add finished task

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,5 +1,25 @@
+function switchCase(letter){
 // Only change code below this line
-
+    var animals;
+    switch(letter){
+        case "a": 
+            animals = "antelope";
+            break;
+        case "b": 
+            animals = "bird";
+            break;
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";    
+            break;
+    }
 // Only change code above this line
+return animals;
+}
+
+console.log(switchCase("4")); //Change this line
+module.exports = switchCase;
 
 
